5e7cd941ddad2fc9dc561bd375abec95dde84aaa
Helion <helionsoftware.llc@gmail.com>
2025-10-30 21:14:48 -0500

    blank static page

---
+++ pages/index.js
@@ -0,0 +1,13 @@
+export default function Home() {
+  return (
+    <div style={{ 
+      display: 'flex', 
+      justifyContent: 'center', 
+      alignItems: 'center', 
+      minHeight: '100vh',
+      fontSize: '24px'
+    }}>
+      This is the first static page
+    </div>
+  )
+}
